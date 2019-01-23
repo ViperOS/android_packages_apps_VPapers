ccb68a4bccaf9f9e8e1323c6f8bb90406a0d6bbf
ZJRDroid <priv.zjr@gmail.com>
2019-01-23 01:28:51 +0000

    VPapers bringup
    Refactor
    
    Create oi.xml
    
    Add wallpapers
    
    Delete oi.xml
    
    Add authors
    
    Add icon

--- src/com/viperos/vpapers/SolidColor.java
+++ src/com/viperos/vpapers/SolidColor.java
@@ -7,7 +7,7 @@
  * or at https://www.gnu.org/licenses/gpl-2.0.txt
  */
 
-package org.aospextended.aexpapers;
+package com.viperos.vpapers;
 
 import android.app.Activity;
 import android.app.AlertDialog;
